d120b84b0df84c7ef2b2b533d318fbc414ee247a
Nizar-bouraoui <nizar.bouraoui7@gmail.com>
2022-02-11 11:54:52 +0000

    add logging for cours

--- src/main/java/com/esprit/examen/services/CoursService.java
+++ src/main/java/com/esprit/examen/services/CoursService.java
@@ -2,24 +2,36 @@ package com.esprit.examen.services;
 
 import java.util.List;
 
-import java.util.Set;
 
+import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.esprit.examen.entities.Cours;
-import com.esprit.examen.entities.Session;
 import com.esprit.examen.repositories.CoursRepository;
-import com.esprit.examen.repositories.SessionRepository;
+
+
 
 @Service
+
 public class CoursService implements ICoursService {
+	
+	private static final org.apache.logging.log4j.Logger l  = LogManager.getLogger(CoursService.class);
+
 
 	@Autowired
 	CoursRepository coursRepository;
+
 	@Override
 	public Long addCours(Cours cours) {
-		coursRepository.save(cours);
+		try {
+			l.info("In addCours()" );
+			l.debug("i will start addCours() ");
+			coursRepository.save(cours);
+			l.debug(" cours added");
+		} catch (Exception e) {
+			l.error("error in addCours() : ", e);
+		}
 		return cours.getId();
 	}
 
@@ -27,26 +39,25 @@ public class CoursService implements ICoursService {
 	public Long modifierCours(Cours cours) {
 		coursRepository.save(cours);
 		return cours.getId();
-		}
+	}
 
 	@Override
 	public void supprimerCours(Long coursId) {
 		coursRepository.deleteById(coursId);
-		
+
 	}
 
 	@Override
 	public List<Cours> getCours() {
-		
-		List<Cours> cours =   coursRepository.findAll();
+
+		List<Cours> cours = coursRepository.findAll();
 		return cours;
 	}
-	
+
 	@Override
-	public void affecterCoursASession(Long coursId, Long sessionId)
-	{
-		/*todo*/
-        
+	public void affecterCoursASession(Long coursId, Long sessionId) {
+		/* todo */
+
 	}
 
 	@Override
@@ -57,7 +68,7 @@ public class CoursService implements ICoursService {
 	@Override
 	public void deleteAll() {
 		coursRepository.deleteAll();
-		
+
 	}
 
 }
--- src/test/java/com/esprit/examen/services/CoursServiceTest.java
+++ src/test/java/com/esprit/examen/services/CoursServiceTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 
 import com.esprit.examen.entities.Cours;
 import com.esprit.examen.entities.TypeCours;
